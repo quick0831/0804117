c25b77c4800e8ff936142611b038257433fed085
Quick <quick0831@gmail.com>
2019-11-07 22:25:25 +0800

    secret_code.cpp 加註解

--- ch8/secret_code.cpp
+++ ch8/secret_code.cpp
@@ -1,40 +1,41 @@
-#include <iostream>
-#include <iomanip>
-#include <cstdlib>
-#include <ctime>
-using namespace std;
-
-main(){
-	int code,input,high=100,low=1;
-	srand((unsigned int)time(NULL)); rand();
-	for(int i=0;i<rand()/10;i++) rand();
-	code=rand()%(high-low-1)+low+1;
-	cout<<"+--------------+"<<endl;
-	cout<<"|              |"<<endl;
-	cout<<"|  終          |"<<endl;
-	cout<<"|        密    |"<<endl;
-	cout<<"|    極        |"<<endl;
-	cout<<"|          碼  |"<<endl;
-	cout<<"|              |"<<endl;
-	cout<<"+--------------+"<<endl<<endl;
-	cout<<"是否要顯示終極密碼？"<<endl;
-	cout<<"(1)是 (2)否      -> ";
-	cin>>input;
-	cout<<endl;
-	if(input==1) cout<<"終極密碼為："<<code<<endl;
-	while(true){
-		cout<<"請做出您的猜測：";
-		cin>>input;
-		if(input<high && input>low){
-			if(input==code) break;
-			if(input<code) low=input;
-			else high=input;
-			cout<<"終極密碼在 "<<low<<" ~ "<<high<<" 之間"<<endl;
-			if(high-low==2) break;
-		}else{
-			cout<<"輸入錯誤，請重試"<<endl;
-		}
-	}
-	cout<<endl<<"遊戲結束，終極密碼為："<<code<<endl<<endl;
-	system("pause");
-}
+#include <iostream>  // 匯入標頭檔 iostream
+#include <iomanip>  // 匯入標頭檔 iomanip
+#include <cstdlib>  // 匯入標頭檔 cstdlib
+#include <ctime>  // 匯入標頭檔 ctime
+using namespace std;  // 宣告命名空間 std
+// 一點空間
+main(){  // 主函數
+	int code,input,high=100,low=1;  // 定義整數變數
+	srand((unsigned int)time(NULL)); rand();  // 初始化亂數
+	for(int i=0;i<rand()/10;i++) rand();  // 繼續洗亂數
+	code=rand()%(high-low-1)+low+1;  // 亂數決定終極密碼
+	cout<<"+--------------+"<<endl;  // 顯
+	cout<<"|              |"<<endl;  // 示
+	cout<<"|  終          |"<<endl;  // 終
+	cout<<"|        密    |"<<endl;  // 極
+	cout<<"|    極        |"<<endl;  // 密
+	cout<<"|          碼  |"<<endl;  // 碼
+	cout<<"|              |"<<endl;  // 的
+	cout<<"+--------------+"<<endl;  // 圖
+	cout<<endl;                      // 案
+	cout<<"是否要顯示終極密碼？"<<endl;  // 顯示功能資訊
+	cout<<"(1)是 (2)否      -> ";  // 顯示選項
+	cin>>input;  // 接收輸入
+	cout<<endl;  // 換行
+	if(input==1) cout<<"終極密碼為："<<code<<endl;  // 輸出終極密碼
+	while(true){  // 迴圈
+		cout<<"請做出您的猜測：";  // 提示文字
+		cin>>input;  // 接收輸入
+		if(input<high && input>low){  // 判斷輸入是否合法
+			if(input==code) break;  // 判斷猜中跳出
+			if(input<code) low=input;  // 猜得太低了
+			else high=input;  // 猜得太高了
+			cout<<"終極密碼在 "<<low<<" ~ "<<high<<" 之間"<<endl;  // 顯示線索
+			if(high-low==2) break;  // 判斷是否只剩一種可能答案
+		}else{  // 否則
+			cout<<"輸入錯誤，請重試"<<endl;  // 錯誤訊息
+		}  // 結束判斷
+	}  // 結束迴圈
+	cout<<endl<<"遊戲結束，終極密碼為："<<code<<endl<<endl;  // 顯示終極密碼
+	system("pause");  // 暫停視窗，方便使用者閱讀
+}  // 程式結束
