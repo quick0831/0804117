accd5cc4bb924e05350a0535a9d4c081024ef666
Quick <quick0831@gmail.com>
2019-11-10 14:42:44 +0800

    prime

--- ch6/6-3-work5.cpp
+++ ch6/6-3-work5.cpp
@@ -4,41 +4,37 @@
 using namespace std;
 
 main(){
-	int input,i,j,total=1,sqrt_i;
-	//const short store_prime_range=168;
-	//short prime[store_prime_range],last=2;
+	int input,i,j,total=0,sqrt_i;
+	const short store_prime_range=168;
+	short prime[store_prime_range],last=2;
 	bool isPrime;
 	cout<<"請輸入一個數字：";
 	cin>>input;
-	cout<<setw(12)<<2;
+	ios_base::sync_with_stdio(false);
+	cin.tie(0);
+	if(input>=2) cout<<" "<<2;
 	for(i=3;i<=input;i+=2){
 		sqrt_i=sqrt(i);
 		if(sqrt_i*sqrt_i==i)
 			isPrime=false;
 		else
 			isPrime=true;
-		for(j=2;j<=sqrt_i && isPrime;j++)
-			if(i%j==0)
-				isPrime=false;
-		/* 
-		for(j=0;j<store_prime_range && j<total;j++){
+		for(j=0;j<store_prime_range && j<total;j++)
 			if(i%prime[j]==0)
 				isPrime=false;
-		}
 		for(j=last+1;j<=sqrt_i && isPrime;j++)
 			if(i%j==0)
 				isPrime=false;
-		*/ 
 		if(isPrime==true){
-			cout<<setw(12)<<i;
-			/* 
+			cout<<" "<<i;
 			if(total<store_prime_range){
 				prime[total]=i;
 				last=i;
-			}*/ 
+			}
 			total++;
 		}
 	}
+	if(input>=2) total++;
 	cout<<endl<<"共"<<total<<"個質數"<<endl; 
 	system("pause");
 }
